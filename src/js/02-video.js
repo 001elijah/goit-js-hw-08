4665c6e048dc42b3e68a89a4d13e2a19bd1f2a00
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-02-26 08:59:08 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,38 +1,33 @@
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-localStorage.setItem("videoplayer-current-time", 0);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
-const timePoint = localStorage.getItem("videoplayer-current-time");
 
-function continueFromCurrentTime() {
-    player.setCurrentTime(timePoint).then(function(timePoint) {
-        // timePoint = the actual time that the player seeked to
-    }).catch(function(error) {
-        switch (error.name) {
-            case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
-                break;
-    
-            default:
-                // some other error occurred
-                break;
-        }
-    });
+function writeCurrentTime(time) {
+    localStorage.setItem(LOCALSTORAGE_KEY, time);
 };
+
 
 player.on('timeupdate', function() {
     player.getCurrentTime().then(function(seconds) {
         // seconds = the current playback position
-
-        localStorage.setItem("videoplayer-current-time", seconds);
-
+        writeCurrentTime(seconds);
     }).catch(function(error) {
         // an error occurred
     });
 });
-window.onbeforeunload = continueFromCurrentTime;
 
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
+window.addEventListener('load', player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) || 0).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+    
+        default:
+            // some other error occurred
+            break;
+    }
+}));
